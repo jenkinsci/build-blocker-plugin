f618f9d144e10299288fb603dd7eea2f90779408
denis mone <monedenis@gmail.com>
2019-12-01 21:33:56 +0200

    Add an additional check for item.ownerTask

--- src/main/java/hudson/plugins/buildblocker/BuildBlockerQueueTaskDispatcher.java
+++ src/main/java/hudson/plugins/buildblocker/BuildBlockerQueueTaskDispatcher.java
@@ -163,9 +163,7 @@ public class BuildBlockerQueueTaskDispatcher extends QueueTaskDispatcher {
         if (checkWasCalledInNodeContext(node) && properties.getBlockLevel().isNode() && !properties.getBlockLevel().isGlobal()) {
             LOG.logp(FINE, getClass().getName(), "checkAccordingToProperties", "calling checkNodeForRunningBuilds");
             Job checkNodeForRunningBuildsResult = jobsMonitor.checkNodeForRunningBuilds(node);
-            if (foundBlocker(checkNodeForRunningBuildsResult)) {
-                return checkNodeForRunningBuildsResult;
-            }
+
             if (properties.getScanQueueFor().isAll()) {
                 LOG.logp(FINE, getClass().getName(), "checkAccordingToProperties", "calling checkNodeForQueueEntries");
                 Job checkNodeForQueueEntriesResult = jobsMonitor.checkNodeForQueueEntries(item, node);
@@ -177,6 +175,10 @@ public class BuildBlockerQueueTaskDispatcher extends QueueTaskDispatcher {
                 Job checkNodeForBuildableQueueEntriesResult = jobsMonitor.checkNodeForBuildableQueueEntries(item, node);
                 if (foundBlocker(checkNodeForBuildableQueueEntriesResult)) {
                     return checkNodeForBuildableQueueEntriesResult;
+                }
+            } else {
+                if (foundBlocker(checkNodeForRunningBuildsResult)) {
+                    return checkNodeForRunningBuildsResult;
                 }
             }
         }
@@ -198,7 +200,11 @@ public class BuildBlockerQueueTaskDispatcher extends QueueTaskDispatcher {
     @CheckForNull
     private BuildBlockerProperty getBuildBlockerProperty(Queue.Item item) {
         if (!(item.task instanceof Job)) {
-            return null;
+            if (!(item.task.getOwnerTask() instanceof Job)) {
+                return null;
+            } else {
+                return ((Job<?, ?>) item.task.getOwnerTask()).getProperty(BuildBlockerProperty.class);
+            }
         }
         Job<?,?> job = (Job<?,?>) item.task;
 
